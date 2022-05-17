bc1bf8a213830b4b4d749c9beb9358814a646d2f
PCOffline <bekerman122@gmail.com>
2022-05-17 18:56:48 +0300

    feat: add App and Home pages

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import Home from './components/Home';
+import './App.css';
+import { AppBar } from '@mui/material';
+
+export default function App() {
+  let location = useLocation();
+  let currentPage = location.pathname;
+
+  return (
+    <>
+      <AppBar id='nav'>
+        <div className='link-container'>
+          <Link to='/' className={currentPage === '/' ? 'active' : ''}>
+            Home
+          </Link>
+        </div>
+        <div className='link-container'>
+          <Link
+            to='/profile'
+            className={currentPage === '/profile' ? 'active' : ''}
+          >
+            Profile
+          </Link>
+        </div>
+      </AppBar>
+      <Outlet />
+    </>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Profile from './components/Profile';
+import Home from './components/Home';
+
+let user = { sum: 50, name: 'Avi' };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Home user={user} />} />
+          <Route  path='profile' element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
