cae47b27a666b82b3b5bebb9bdb7834f0d3060eb
PCOffline <bekerman122@gmail.com>
2022-05-21 02:13:31 +0300

    feat: color palettes & routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
+import App from './App';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Profile from './components/pages/Home/Profile';
-// import Home from './components/pages/Home';
+import Profile from './components/pages/Home/Profile';
+import Home from './components/pages/Home';
 import SignUp from './components/pages/SignUp';
 import Login from './components/pages/Login';
 import { createTheme, ThemeProvider } from '@mui/material';
@@ -14,11 +14,13 @@ let user = { sum: 50, name: 'Avi' };
 const theme = createTheme({
   palette: {
     primary: {
+      light: 'rgba(0, 171, 85, 0.08)',
       main: '#00ab55',
       dark: '#007B55',
     },
     secondary: {
-      main: '#00ab55',
+      light: 'rgba(145, 158, 171, 0.08)',
+      main: '#919eab',
     },
     text: {
       primary: '#212b36',
@@ -35,11 +37,12 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path='/signup' element={<SignUp />} />
-          <Route path='/login' element={<Login  />} />
-          {/* <Route path='/' element={<App />}>
-          <Route index element={<Home user={user} />} />
-          <Route  path='profile' element={<Profile />} />
-        </Route> */}
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<App />}>
+            <Route index element={<Home user={user} />} />
+            <Route path='transactions' element={<p>Hi</p>}  />
+            <Route path='profile' element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
