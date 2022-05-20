24024585ec025299dc442711a49f3e406f5d3c2e
PCOffline <bekerman122@gmail.com>
2022-05-21 02:13:17 +0300

    feat: app nav bar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,68 @@
-import { Link, Outlet, useLocation } from 'react-router-dom';
-import Home from './components/pages/Home';
-import './App.css';
-import { AppBar } from '@mui/material';
+import { Link, NavLink, Outlet, useLocation } from 'react-router-dom';
+import {
+  AppBar,
+  List,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import styled from '@emotion/styled';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import BarChartRoundedIcon from '@mui/icons-material/BarChartRounded';
+import CurrencyExchangeRoundedIcon from '@mui/icons-material/CurrencyExchangeRounded';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+
+const NavBar = styled(List)(({ theme }) => ({
+  flexDirection: 'column',
+  backgroundColor: '#fff',
+  color: theme.palette.text.disabled,
+  width: '20%',
+}));
+
+const ActiveNav = styled(NavLink)(({ theme }) => ({
+  textDecoration: 'none',
+  color: theme.palette.text.disabled,
+
+  '&.active': {
+    color: theme.palette.primary.main,
+  },
+  '&.active > div': {
+    backgroundColor: theme.palette.primary.light,
+  },
+  '&.active > div > div': {
+    color: theme.palette.primary.main,
+  },
+  '&:hover > div': {
+    backgroundColor: theme.palette.secondary.light,
+  },
+}));
 
 export default function App() {
-  let location = useLocation();
-  let currentPage = location.pathname;
+  const renderNav = (route, text, icon) => {
+    return (
+      <ActiveNav to={route}>
+        <ListItemButton>
+          <ListItemIcon>{icon}</ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItemButton>
+      </ActiveNav>
+    );
+  };
 
   return (
     <>
-      <AppBar id='nav'>
-        <div className='link-container'>
-          <Link to='/' className={currentPage === '/' ? 'active' : ''}>
-            Home
-          </Link>
-        </div>
-        <div className='link-container'>
-          <Link
-            to='/profile'
-            className={currentPage === '/profile' ? 'active' : ''}
-          >
-            Profile
-          </Link>
-        </div>
-      </AppBar>
+      <NavBar id='nav'>
+        {renderNav('/', 'Home', <HomeRoundedIcon />)}
+        {renderNav('transactions', 'Transactions', <BarChartRoundedIcon />)}
+        {renderNav(
+          '/lends-and-borrowings',
+          'Lends & Borrowings',
+          <CurrencyExchangeRoundedIcon />,
+        )}
+        {renderNav('/profile', 'Profile', <PersonRoundedIcon />)}
+      </NavBar>
       <Outlet />
     </>
   );
