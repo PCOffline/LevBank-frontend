0481a9b97a859e6f7e8f12f8fab13e935aea545d
PCOffline <bekerman122@gmail.com>
2022-05-21 19:08:02 +0300

    feat: navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link, NavLink, Outlet, useLocation } from 'react-router-dom';
 import {
   AppBar,
@@ -7,21 +8,27 @@ import {
   ListItemIcon,
   ListItemText,
   Typography,
+  Box,
+  Card,
+  Avatar,
 } from '@mui/material';
 import styled from '@emotion/styled';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import BarChartRoundedIcon from '@mui/icons-material/BarChartRounded';
 import CurrencyExchangeRoundedIcon from '@mui/icons-material/CurrencyExchangeRounded';
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import Logo from './components/common/Logo';
+import FaceIcon from '@mui/icons-material/Face';
 
 const NavBar = styled(List)(({ theme }) => ({
   flexDirection: 'column',
   backgroundColor: '#fff',
   color: theme.palette.text.disabled,
-  width: '20%',
+  width: '240px',
+  padding: '0 16px',
 }));
 
-const ActiveNav = styled(NavLink)(({ theme }) => ({
+const NavItem = styled(NavLink)(({ theme }) => ({
   textDecoration: 'none',
   color: theme.palette.text.disabled,
 
@@ -39,21 +46,62 @@ const ActiveNav = styled(NavLink)(({ theme }) => ({
   },
 }));
 
+const NavItemContainer = styled(ListItemButton)(({ theme }) => ({
+  borderRadius: '12px',
+  padding: '8px 12px 8px 16px',
+}));
+
+const TopNav = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '24px 0 16px 0',
+  gap: '2em',
+  marginBottom: '2em',
+}));
+
+const ProfileName = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.primary,
+  fontWeight: 600,
+}));
+
+const ProfileBalance = styled(Typography)(({ theme }) => ({
+  fontWeight: 400,
+}));
+
+const ProfileContainer = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.light,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  padding: '10px 16px',
+  borderRadius: '8px',
+}));
+
+const ChatContainer = styled(Box)(({ theme }) => ({}));
+
 export default function App() {
   const renderNav = (route, text, icon) => {
     return (
-      <ActiveNav to={route}>
-        <ListItemButton>
+      <NavItem to={route}>
+        <NavItemContainer>
           <ListItemIcon>{icon}</ListItemIcon>
           <ListItemText primary={text} />
-        </ListItemButton>
-      </ActiveNav>
+        </NavItemContainer>
+      </NavItem>
     );
   };
 
   return (
     <>
       <NavBar id='nav'>
+        <TopNav>
+          <Logo withText />
+          <ProfileContainer>
+            <ProfileName>Avi Levi</ProfileName>
+            <ProfileBalance>129 LC</ProfileBalance>
+          </ProfileContainer>
+        </TopNav>
         {renderNav('/', 'Home', <HomeRoundedIcon />)}
         {renderNav('transactions', 'Transactions', <BarChartRoundedIcon />)}
         {renderNav(
@@ -62,6 +110,7 @@ export default function App() {
           <CurrencyExchangeRoundedIcon />,
         )}
         {renderNav('/profile', 'Profile', <PersonRoundedIcon />)}
+        <ChatContainer></ChatContainer>
       </NavBar>
       <Outlet />
     </>
