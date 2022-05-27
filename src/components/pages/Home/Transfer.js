e8472926a973e61c8e13f295b5cd9907756adda3
PCOffline <bekerman122@gmail.com>
2022-05-27 15:36:52 +0300

    feat: finish Transfer

--- src/components/pages/Home/Transfer.js
+++ src/components/pages/Home/Transfer.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Typography, Card, MenuItem, Select, TextField, Input, Button } from '@mui/material';
+import { Typography, Card, MenuItem, Select, TextField, Input, Button, OutlinedInput } from '@mui/material';
 import styled from '@emotion/styled';
 
 const Container = styled(Card)(({ theme }) => ({
@@ -19,12 +19,15 @@ const Title = styled(Typography)(({ theme }) => ({
 
 
 export default function Transfer(props) {
+  const [recipient, setRecipient] = useState('');
+  const [amount, setAmount] = useState(1);
+
   return (
     <Container>
       <Title>Transfer Money</Title>
-      <TextField placeholder='Receiving account'></TextField>
-      <Input type='number' placeholder='Sum' inputProps={{ min: 1, max: props.user.balance }} />
-      <Button variant='contained'>Send</Button>
+      <TextField placeholder='Receiving account' value={recipient} onChange={(event) => setRecipient(event.target.value)} />
+      <OutlinedInput type='number' placeholder='Sum' value={amount} onChange={(event) => setAmount(+event.target.value)}inputProps={{ min: 1, max: props.user.balance }} />
+      <Button variant='contained' disabled={!recipient.trim() || amount > props.user.balance || amount <= 0 || !Number.isSafeInteger(amount)}>Send</Button>
     </Container>
   );
 }
