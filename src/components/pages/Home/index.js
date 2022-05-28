25d821a05e71c4784cb52278c4bbbedf30aef88e
PCOffline <bekerman122@gmail.com>
2022-05-29 01:16:57 +0300

    feat(Home): exchange & stylings

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -3,18 +3,35 @@ import Transactions from './Transactions';
 import styled from '@emotion/styled';
 import { Box } from '@mui/material';
 import Transfer from '../../common/Transfer';
+import InfoCard from '../../common/InfoCard';
+import Exchange from './Exchange';
 
 const Container = styled(Box)(({ theme }) => ({
   display: 'flex',
   marginTop: '2rem',
-  width: '80%',
+  width: 'fit-content',
+  paddingRight: '3rem',
   height: 'fit-content',
-  justifyContent: 'space-around',
+  justifyContent: 'space-between',
   gap: '1rem',
+  flexWrap: 'wrap',
   [theme.breakpoints.down('lg')]: {
-    flexWrap: 'wrap',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  '& div': {
+    height: 'auto',
+  },
+}));
+
+const StyledTransactions = styled(Transactions)(({ theme }) => ({
+  maxHeight: '40vh',
+  [theme.breakpoints.down('xl')]: {
+    maxHeight: '50vh',
   }
+}));
+
+const StyledInfoCard = styled(InfoCard)(({ theme }) => ({
+  maxWidth: '30%',
 }));
 
 export default function Home(props) {
@@ -22,7 +39,22 @@ export default function Home(props) {
     <Container>
       <UserInfo user={props.user} />
       <Transfer user={props.user} />
-      <Transactions user={props.user} transactions={props.user.transactions} currency='LC' />
+      <Exchange />
+      <StyledTransactions
+        user={props.user}
+        transactions={props.user.transactions}
+        currency='LC'
+      />
+      <StyledInfoCard
+        title='Home Page'
+        details={[
+          'View a summary of your account and recent transactions.',
+          'Transfer money to different users by typing their username and the amount you wish to transfer.',
+          'You can also view the current exchange rates for the currencies you have selected.',
+          'For more information, contact an administrator.'
+        ]}
+        onChatClick={() => console.log('hello')}
+      />
     </Container>
   );
 }
